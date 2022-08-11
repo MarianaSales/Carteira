0bbb48b07c4b9e8a1f780f61b283a62f45b174e9
MarianaSales <38512371+MarianaSales@users.noreply.github.com>
2022-08-11 15:53:55 -0300

    correção do Date e mudança de context para context selector
    evitar renderizar informação desnecessária

--- src/contexts/TransactionsContexts.tsx
+++ src/contexts/TransactionsContexts.tsx
@@ -1,63 +1,64 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
-import { api } from '../lib/axios'
+import { ReactNode, useEffect, useState } from 'react';
+import { createContext } from 'use-context-selector';
+import { api } from '../lib/axios';
 
 interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createdAt: string
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: Date;
 }
 
 interface CreateTransactionInput {
-  description: string
-  price: number
-  category: string
-  type: 'income' | 'outcome'
+    description: string;
+    price: number;
+    category: string;
+    type: 'income' | 'outcome';
 }
 interface TransactionsContextType {
-  transactions: Transaction[]
-  fetchTransactions: () => Promise<void>
-  createTransaction: (data: CreateTransactionInput) => Promise<void>
+    transactions: Transaction[];
+    fetchTransactions: () => Promise<void>;
+    createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 interface TransactionsProviderProps {
-  children: ReactNode
+    children: ReactNode;
 }
 
-export const TransactionsContext = createContext({} as TransactionsContextType)
+export const TransactionsContext = createContext({} as TransactionsContextType);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
-    const response = await api.get('/transactions', {
-      params: {
-        _sort: 'createAt',
-        _order: 'desc',
-        q: query,
-      },
-    })
-    setTransactions(response.data)
-  }
+    async function fetchTransactions(query?: string) {
+        const response = await api.get('/transactions', {
+            params: {
+                _sort: 'createAt',
+                _order: 'desc',
+                q: query,
+            },
+        });
+        setTransactions(response.data);
+    }
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const response = await api.post('/transactions', {
-      description: data.category,
-      price: data.price,
-      category: data.category,
-      type: data.type,
-      createAt: new Date(),
-    })
-    setTransactions((state) => [response.data, ...state])
-  }
-  useEffect(() => {
-    fetchTransactions()
-  }, [])
-  return (
-    <TransactionsContext.Provider
-      value={{ transactions, fetchTransactions, createTransaction }}>
-      {children}
-    </TransactionsContext.Provider>
-  )
+    async function createTransaction(data: CreateTransactionInput) {
+        const response = await api.post('/transactions', {
+            description: data.description,
+            price: data.price,
+            category: data.category,
+            type: data.type,
+            createAt: new Date(),
+        });
+        setTransactions((state) => [response.data, ...state]);
+    }
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
+    return (
+        <TransactionsContext.Provider
+            value={{ transactions, fetchTransactions, createTransaction }}>
+            {children}
+        </TransactionsContext.Provider>
+    );
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,44 +1,42 @@
-import { useContext } from 'react'
-import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary'
-import { TransactionsContext } from '../../contexts/TransactionsContexts'
-import { dateFormatter, priceFormatter } from '../../utils/formatter'
-import { SearchForm } from './SearchForm'
-import {
-  PriceHighlight,
-  TransactionContainer,
-  TransactionTable,
-} from './styles'
+import { useContextSelector } from 'use-context-selector';
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { TransactionsContext } from '../../contexts/TransactionsContexts';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
+import { SearchForm } from './components/SearchForm';
+import { PriceHighlight, TransactionContainer, TransactionTable } from './styles';
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+    const transactions = useContextSelector(TransactionsContext, (context) => {
+        return context.transactions;
+    });
 
-  return (
-    <div>
-      <Header />
-      <Summary />
-      <TransactionContainer>
-        <SearchForm />
-        <TransactionTable>
-          <tbody>
-            {transactions.map((transaction) => {
-              return (
-                <tr key={transaction.id}>
-                  <td width="50%">{transaction.description}</td>
-                  <td>
-                    <PriceHighlight variant={transaction.type}>
-                      {transaction.type === 'outcome' && '- '}
-                      {priceFormatter.format(transaction.price)}
-                    </PriceHighlight>
-                  </td>
-                  <td>{transaction.category}</td>
-                  <td>{dateFormatter.format(transaction.createdAt)}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </TransactionTable>
-      </TransactionContainer>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <Summary />
+            <TransactionContainer>
+                <SearchForm />
+                <TransactionTable>
+                    <tbody>
+                        {transactions.map((transaction) => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{dateFormatter.format(transaction.createdAt)}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
+        </div>
+    );
 }
